da91103a95fd4cc52ef236b9ed8aa9bd3bf2feb6
BlingYang <20971840@qq.com>
2024-08-07 17:38:13 +0800

    1、增加恐龙快打、三国战纪、三国志、拳皇94、95、96、97、98、99、2000、2001、2002、2003

--- gameEntrance/classic/[game].md
+++ gameEntrance/classic/[game].md
@@ -7,6 +7,7 @@ footer: false
 <script setup>
 import { useData } from 'vitepress'
 import { ref } from 'vue'
+import virtureroute from '../../.vitepress/config/virtureroute.json'
 
 // params 是一个 Vue ref
 const { params } = useData()
@@ -21,14 +22,22 @@ if (params.value.game === 'xxx'){
 }
 id.value = "h5" + params.value.game;
 if (params.value.game === 'redalert2') {
-    gameurl.value = "https://www.ra2web.com/";
+    gameurl.value = "https://www.ra2web.com/";//调用外部页面入口
 }else if (params.value.game === 'h5battlecity') {
-    gameurl.value = "/classic/h5battlecity/index.html"
+    gameurl.value = "/classic/h5battlecity/index.html" //本地h5游戏文件
 }else if (params.value.game === 'h5mario') {
     gameurl.value = '/classic/h5mario/index.html'
-}else {
-    gameurl.value = "/classic/games/index.html?language=zh-CN&name=" + params.value.game;
+}else {//模拟器文件入口
+    let iscdnfile = false;
+    virtureroute.forEach(function(element){
+        if (element.filename === params.value.game) {
+            iscdnfile = true;
+        }
+    })
+    gameurl.value = "/classic/games/index.html?iscdnfile=" + iscdnfile + 
+                    "&language=zh-CN&name=" + params.value.game;
 }
+    //console.log(gameurl);
 </script>
 
 <GameEntranceV :id="id" :src="gameurl" :resetHeight="resetHeight"></GameEntranceV>
--- gameEntrance/classic/[game].paths.ts
+++ gameEntrance/classic/[game].paths.ts
@@ -1,18 +1,22 @@
 import { filepaths } from '../../.vitepress/utils.mjs'
+import virtureroute from '../../.vitepress/config/virtureroute.json'
 
 export default {
   paths() { 
-    let basepath = filepaths('./public/classic')
-    let nesfilepath = filepaths('./public/classic/games/nesfile');
-    let gbafilepath = filepaths('./public/classic/games/mame2003plusfile');
+    let basepath = filepaths('./public/classic')//获取classic的子文件夹，本地h5游戏存放在这里
+    let nesfilepath = filepaths('./public/classic/games/nesfile');//nes文件
+    let gbafilepath = filepaths('./public/classic/games/mame2003plusfile');//zip文件
     let filepath = basepath;
     filepath = filepath.concat(nesfilepath);
     filepath = filepath.concat(gbafilepath);
-    filepath.push({params:{game: 'redalert2'}});//嵌入其他网页的路由
-    filepath.push({params:{game: 'mslug3.zip'}});//访问CDN路径的路由
-    filepath.push({params:{game: 'mslug4.zip'}});
-    filepath.push({params:{game: 'mslugx.zip'}});
-    //console.log(filepath);
+    virtureroute.forEach(function(element){//添加cdn文件名称
+      filepath.push({params:{game: element.filename}})
+    });
+    // filepath.push({params:{game: 'redalert2'}});//嵌入其他网页的路由
+    // filepath.push({params:{game: 'mslug3.zip'}});//访问CDN路径的路由
+    // filepath.push({params:{game: 'mslug4.zip'}});
+    // filepath.push({params:{game: 'mslugx.zip'}});
+    console.log(filepath);
     return filepath;
   }
 }
